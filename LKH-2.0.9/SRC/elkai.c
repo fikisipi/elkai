c4844a6ccac1fbd1b7ac2823140917905efa4ec6
Mathias Rav <m@git.strova.dk>
2019-05-18 20:13:14 +0200

    Switch from Python.h to milksnake+cffi
    This allows `setup.py bdist_wheel` to create a wheel that is independent
    of the Python version.
    
    Move `InvokeSolver()` from `_elkai.c` into `LKH-2.0.9/SRC/elkai.c`,
    and reimplement `solve()` in `elkai/_elkai.py`.
    
    Update `LKH-2.0.9/SRC/Makefile` to build a shared library named
    `libelkai.so` (instead of trying to build an executable without `main`).
    
    Update `setup.py` to invoke `make` and generate CFFI bindings for
    `LKH-2.0.9/SRC/INCLUDE/elkai.h` (which only declares `InvokeSolver()`).

--- LKH-2.0.9/SRC/elkai.c
+++ LKH-2.0.9/SRC/elkai.c
@@ -1,4 +1,3 @@
-#include <Python.h>
 #include "LKH.h"
 #include "Genetic.h"
 #include "math.h"
@@ -11,89 +10,6 @@
 // - LKH feature: Add known optimal subtours?
 // - Make sure camel case is used everywhere
 // - Use git submodule instead of a LKH copy
-
-static int InvokeSolver(int *matrixBuff, int matrixLen, int runCount, int *tourBuff, int *tourN);
-
-static PyObject *ElkSolve(PyObject *self, PyObject *args)
-{
-    // *args of a vararg Python function is a tuple
-    // of unknown length.
-
-    if(PyObject_Length(args) != 2) {
-        PyErr_SetString(PyExc_TypeError, "Expected two arguments");
-        return 0;
-    }
-
-    PyObject *arg = PyObject_GetItem(args, PyLong_FromLong(0));
-    PyObject *runArg = PyObject_GetItem(args, PyLong_FromLong(1));
-
-    long runCount = PyLong_AsLong(runArg);
-    if(PyErr_Occurred() != 0 || runCount <= 0) {
-        PyErr_SetString(PyExc_TypeError, "Second argument must be a positive int");
-        return 0;
-    } 
-
-    int pyLen = PyObject_Length(arg);
-
-    // We get the problem dimension as a square root of the flat NxN
-    // matrix length. It could be done using recursive bit shifts too.
-
-    int pyLenSqrt = (int)sqrt(pyLen);
-    if (pyLen < 4 || pyLenSqrt * pyLenSqrt != pyLen)
-    {
-        PyErr_SetString(PyExc_TypeError, "Argument must be a list of integers with N^2 >= 4 elements.\n"
-                                          "Example: [1, 1, 1, 1].");
-        return 0;
-    }
-    int *matrixBuff = (int *)malloc(sizeof(int) * pyLen);
-    int *tourBuff = (int *)malloc(sizeof(int) * pyLenSqrt);
-    int tourN = 0;
-
-    long i;
-    for (i = 0; i < pyLen; i++)
-    {
-        PyObject *pyNumber = PyObject_GetItem(arg, PyLong_FromLong(i));
-        long justNumber = PyLong_AsLong(pyNumber);
-        if(PyErr_Occurred() != 0) {
-            PyErr_SetString(PyExc_TypeError, "List must only contain integers");
-            return 0;
-        }
-        int justNumber_i = (int)justNumber;
-        // TODO: don't lose precision
-        matrixBuff[i] = justNumber_i;
-    }
-    int norm_result = InvokeSolver(matrixBuff, pyLen, runCount, tourBuff, &tourN);
-    free(matrixBuff);
-    PyObject *list = PyList_New(tourN);
-    for (i = 0; i < tourN; i++)
-    {
-        PyObject *tourElement = PyLong_FromLong((long)tourBuff[i]);
-        PyList_SetItem(list, i, tourElement);
-    }
-
-    free(tourBuff);
-    return list;
-}
-
-static char elk_docs[] =
-    "solve(x): Solve a TSP problem.\n";
-
-static PyMethodDef funcs[] = {
-    {"solve", (PyCFunction) ElkSolve,
-     METH_VARARGS, elk_docs},
-    {NULL}};
-
-static struct PyModuleDef elkDef = {
-    PyModuleDef_HEAD_INIT,
-    "_elkai",
-    "",
-    -1,
-    funcs};
-
-PyMODINIT_FUNC PyInit__elkai(void)
-{
-    return PyModule_Create(&elkDef);
-}
 
 static void LoadWeightMatrix(int *arr);
 
@@ -483,7 +399,7 @@ void _Reset8();
 
 // InvokeSolver reads the matrix buffer and outputs it into tourBuff and updates
 // tourN which is the tour length.
-static int InvokeSolver(int *matrixBuff, int matrixLen, int runCount, int *tourBuff, int *tourN)
+int InvokeSolver(int *matrixBuff, int matrixLen, int runCount, int *tourBuff, int *tourN)
 {
     _Reset1();
     _Reset2();
