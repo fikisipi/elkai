348686ffa0570f584fd1df22f784d7c6ffcf1946
Filip Dimitrovski <filipdimitrovski22@gmail.com>
2019-10-24 20:33:30 +0200

    Reuse CMake from 3.7 in 3.8

--- build_wheels.sh
+++ build_wheels.sh
@@ -2,12 +2,22 @@
 set -e -x
 
 # Install a system package required by our library
-yum install -y atlas-devel cmake
+yum install -y atlas-devel
 
 MPATH="$PATH"
+# Reuse 3.7 CMake for 3.8
+for PYBIN in /opt/python/cp37*/bin; do
+    "${PYBIN}/python" -m pip install cmake
+    cp -r /opt/python/cp37*/lib/python3.7/site-packages/cmake /opt/python/cp38-cp38/lib/python3.8/site-packages/
+    cp /opt/python/cp37*/bin/cmake /opt/python/cp38-cp38/bin
+done
+
 # Compile wheels
 for PYBIN in /opt/python/cp3*/bin; do
     PATH="${PYBIN}/:${MPATH}"
+    if [[ $PYBIN != *"cp38"* ]]; then
+          "${PYBIN}/python" -m pip install cmake
+    fi
     "${PYBIN}/python" -m pip install scikit-build ninja
     "${PYBIN}/python" -m pip wheel /io/ -w dist/
     /bin/rm -rf /io/_skbuild
